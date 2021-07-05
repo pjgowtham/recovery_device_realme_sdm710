142f1f9ca532b0f0fa53101115a565b13625155f
pjgowtham <pjgowtham@gmail.com>
2021-07-05 13:08:30 +0530

    realme_sdm710 : Intial skeleton * References from current tree for android-9 branch * https://github.com/bigbiff/android_device_oneplus_kebab * Set default brightness to 230, matching stock values * Disable screen time out/screen off due to unresponsive touch * Kernel is unified to fix touchscreen for realme 5 pro https ://github.com/pjgowtham/kernel_realme_sdm710 * fstab entries for otg and sdcard1 added to support realme 3 pro * Match init.recovery.qcom.rc from CAF * ueventd.rc pulled from stock RUI2 * Manifest pulled from stock RUI2

---
+++ BoardConfig.mk
@@ -0,0 +1,146 @@
+#
+# Copyright (C) 2019 The TwrpBuilder Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/realme/realme_sdm710
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a9
+
+ENABLE_CPUSETS := true
+ENABLE_SCHEDBOOST := true
+
+# Platform
+TARGET_BOARD_PLATFORM := sdm710
+TARGET_BOOTLOADER_BOARD_NAME := sdm710
+TARGET_NO_BOOTLOADER := true
+TARGET_USES_UEFI := true
+
+# fstab
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery.fstab
+
+# Kernel
+BOARD_KERNEL_CMDLINE := \
+   console=ttyMSM0,115200n8 \
+   earlycon=msm_geni_serial,0xA90000 \
+   androidboot.hardware=qcom \
+   androidboot.console=ttyMSM0 \
+   video=vfb:640x400,bpp=32,memsize=3072000 \
+   msm_rtb.filter=0x237 \
+   ehci-hcd.park=3 \
+   lpm_levels.sleep_disabled=1 \
+   service_locator.enable=1 \
+   androidboot.configfs=true \
+   androidboot.usbcontroller=a600000.dwc3 \
+   swiotlb=1 \
+   loop.max_part=7
+#   kpti=off \
+
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_NO_KERNEL := false
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz-dtb
+#BOARD_KERNEL_SEPARATED_DTBO := true
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
+BOARD_INCLUDE_RECOVERY_DTBO := true
+BOARD_MKBOOTIMG_ARGS += --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --header_version 1
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := RMX1921,RMX1921EU,RMX1901,RMX1901CN,RMX1851,RMX1971,RMX1971CN
+
+# Partitions
+BOARD_FLASH_BLOCK_SIZE := 262144
+BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 5435817984
+BOARD_SYSTEMIMAGE_PARTITION_TYPE := ext4
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 55141412864
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDORIMAGE_PARTITION_SIZE := 1660944384
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+#BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
+#BOARD_ODMIMAGE_PARTITION_SIZE := 268435456
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+
+TARGET_USERIMAGES_USE_EXT4 := true
+
+# Workaround for error copying vendor files to recovery ramdisk
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR := vendor
+
+# Recovery
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+TARGET_NO_RECOVERY := false
+
+# Use mke2fs to create ext4 images
+TARGET_USES_MKE2FS := true
+
+# Encryption
+PLATFORM_VERSION := 16.1.0
+PLATFORM_SECURITY_PATCH := 2099-12-31
+VENDOR_SECURITY_PATCH := 2099-12-31
+#TW_INCLUDE_CRYPTO := true
+#TW_INCLUDE_CRYPTO_FBE := true
+#BOARD_USES_QCOM_FBE_DECRYPTION := true
+
+# Extras
+TW_INCLUDE_RESETPROP := true
+TW_INCLUDE_REPACKTOOLS := true
+TW_INCLUDE_LIBRESETPROP := true
+
+# TWRP specific build flags
+TW_DEVICE_VERSION := unified_beta1
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file
+TW_THEME := portrait_hdpi
+TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
+TW_DEFAULT_BRIGHTNESS := 230
+TW_MAX_BRIGHTNESS := 1023
+TW_Y_OFFSET := 80
+TW_H_OFFSET := -80
+TW_NO_SCREEN_TIMEOUT := true
+TW_NO_SCREEN_BLANK := true
+TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_EXCLUDE_ENCRYPTED_BACKUPS := true
+TW_EXCLUDE_TWRPAPP := true
+TW_EXTRA_LANGUAGES := true
+TW_HAS_EDL_MODE := true
+TW_INCLUDE_NTFS_3G := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_NO_BIND_SYSTEM := true
+TW_NO_EXFAT_FUSE := true
+
+# TWRP Debug Flags
+TARGET_USES_LOGD := true
+#TWRP_EVENT_LOGGING := true
+TWRP_INCLUDE_LOGCAT := true
+TARGET_RECOVERY_DEVICE_MODULES += debuggerd
+TW_RECOVERY_ADDITIONAL_RELINK_FILES += $(TARGET_OUT_EXECUTABLES)/debuggerd
---
+++ README.md
@@ -0,0 +1,40 @@
+# android_device_realme_(realme_sdm710)
+Recovery tree for realme sdm710 devices
+
+## Features
+
+Works:
+
+- Everything except for screen timeout/lockscreen which is intentionally made not to timeout because of unresponsive touch.
+
+## Compile
+
+First checkout manifest :
+
+```
+repo init --depth=1 -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git -b twrp-11
+repo sync
+```
+
+Then clone the current device tree onto device/realme/realme_sdm710
+
+
+Finally execute these:
+
+```
+. build/envsetup.sh
+lunch omni_RMX1921-eng
+mka recoveryimage ALLOW_MISSING_DEPENDENCIES=true
+```
+
+To test it:
+
+```
+fastboot flash /path/to/recovery.img
+```
+
+
+## Credits
+
+- Thanks to @mauronfrio for the TWRP tree for realme X/XT
+- TWRP team
